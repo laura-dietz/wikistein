d5f8ed0bfb8d026bb4ff73d0539354ae49cb81eb
Laura Dietz <dietz@cs.umass.edu>
2017-01-28 13:38:46 -0500

    setup wikistein distribution

--- run.sh
+++ run.sh
@@ -2,7 +2,12 @@
 
 #wget http://trec-car.cs.unh.edu/datareleases/spritzer-v1.4.zip
 #unzip spritzer-v1.4.zip -d data
-git clone https://github.com/laura-dietz/wikistein.git
+
+if [[ -d wikistein ]]; then
+    cd wikistein; git pull; cd ..
+else
+    git clone https://github.com/laura-dietz/wikistein.git
+fi
 
 cd wikistein
 python setup.py install
